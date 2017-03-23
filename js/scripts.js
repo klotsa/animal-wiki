37a3d3a9400a185231b12da7fabed995fd3848d5
Anatoliy Klots and Marcel Nogueira <marcel@>
2017-03-23 10:38:32 -0700

    create three divs with animal info launched by users choice

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+
+  $("#showCat").click(function() {
+    $("#hiddenCats").toggle();
+  });
+
+  $("#showDog").click(function() {
+    $("#hiddenDogs").toggle();
+  });
+
+  $("#showBird").click(function() {
+    $("#hiddenBirds").toggle();
+  });
+});  
